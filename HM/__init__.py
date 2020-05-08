14bc48c14b41c6a1132932d3fa0d67475f9f2d02
karenw1004 <karenw0103@gmail.com>
2020-05-08 12:42:44 -0500

    Initial commit v2

---
+++ HM/__init__.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from flask_login import LoginManager
+from HM.core.views import core
+# from HM.users.views import users
+from HM.error_pages.handlers import error_pages
+from HM.database import *
+
+# Initialisation
+database().__init__
+app = Flask(__name__)
+
+app.register_blueprint(core)
+# app.register_blueprint(users)
+app.register_blueprint(error_pages)
+
+
+
+
+# @app.route('/')
+# def login():
+
+# from database import *
+# from flask import Flask, render_template
+
+
+# # Initialisation
+# database().__init__
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def login():
+    
+    
---
+++ HM/database.py
@@ -0,0 +1,46 @@
+import datetime
+import mysql.connector
+
+class database:
+    #mysql test
+    #source: https://www.youtube.com/watch?v=xgyVilYfJEo, https://dev.mysql.com/doc/connector-python/en/connector-python-api-errors-error.html
+    def __init__(self):
+        self.conn = 0
+        self.mycursor = 0
+        self.working = False
+        try:
+            self.conn=mysql.connector.connect(user='karenws',password='eiR4eiyi',host='mysql.eecs.ku.edu',database='karenws')
+            self.mycursor=self.conn.cursor(buffered=True)
+            self.mycursor.execute("SHOW TABLES")            
+            print(self.mycursor.fetchall())
+            self.working = True
+        except ImportError:
+            print("database() error: MySQL-Connector could not be imported")
+        except mysql.connector.Error as err:
+            print("database() error: {}".format(err))
+
+    # def addScore(self, score):
+    #     """take score and put it into the database
+    #     """
+    #     if(self.working):
+    #         add_score = ("INSERT INTO scores_alltime (score, name, timestamp) VALUES (%s, %s, %s)")
+    #         score_data = (score, 'test', datetime.datetime.now())
+    #         self.mycursor.execute(add_score, score_data)
+    #     #    self.printTable()
+
+    # def printTable(self):
+    #     """print all the scores in the high scores table
+    #     """
+    #     if(self.working):
+    #         self.mycursor.execute("SELECT score "
+    #             "FROM scores_alltime "
+    #             "ORDER BY score DESC "
+    #             "LIMIT 0 , 30")
+    #         #print(self.mycursor.fetchall())
+    #         return self.mycursor.fetchall()
+    #     return "COULD NOT CONNECT TO DATABASE"
+    #     """to be implemented...
+    #     self.mycursor.execute("SELECT COUNT(*) FROM fooTable")
+    #             UPDATE table_name
+    #     SET column1=value1,column2=value2,...
+    #     WHERE some_column=some_value;"""
